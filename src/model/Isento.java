2309e789db3f9ca37b67b693e3dab4e46e42b84f
unknown <kaiqueedu94@gmail.com>
2018-08-17 23:47:59 -0300

    POO_Teatro

---
+++ src/model/Isento.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Isento extends Ingresso{
+    
+    public Isento(Cadeira cadeira) {
+        super(cadeira);
+    }    
+    @Override
+    public double getValor() {
+        return 0;
+    }
+}
---
+++ src/view/Main.java
@@ -0,0 +1,48 @@
+package view;
+
+import javax.swing.JOptionPane;
+import model.Vendas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Vendas vendas;
+        int fileira;
+        int assento;
+        int tipoIngresso;
+        boolean vendeu;
+        vendas = new Vendas();
+        do {
+            JOptionPane.showMessageDialog(null, "Venda de ingressos", "Ingressos do Teatro POO",
+                    JOptionPane.INFORMATION_MESSAGE);
+            fileira = Integer.parseInt(JOptionPane.showInputDialog("Fileira"));
+            assento = Integer.parseInt(JOptionPane.showInputDialog("Cadeira"));
+            
+            tipoIngresso = JOptionPane.showConfirmDialog(null, "Ingresso Isento?",
+                    "Ingressos do Teatro POO", JOptionPane.YES_NO_OPTION);         
+                  
+            if(tipoIngresso == JOptionPane.YES_OPTION){
+                vendeu = vendas.novaVenda(fileira, assento, Vendas.ISENTO);                
+            }else{                 
+                tipoIngresso = JOptionPane.showConfirmDialog(null, "Ingresso preço cheio?",
+                    "Ingressos do Teatro POO", JOptionPane.YES_NO_OPTION);
+                if (tipoIngresso == JOptionPane.YES_OPTION) {
+                vendeu = vendas.novaVenda(fileira, assento, Vendas.INTEIRA);
+                } else {
+                vendeu = vendas.novaVenda(fileira, assento, Vendas.MEIA);
+                }
+            }          
+            
+            if (!vendeu) {
+                JOptionPane.showMessageDialog(null, "Cadeira ocupada", "Ingresso do Teatro POO",
+                        JOptionPane.ERROR_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Bom espetáculo", "Ingresso do Teatro POO",
+                        JOptionPane.WARNING_MESSAGE);
+            }
+        } while (JOptionPane.showConfirmDialog(null, "Deseja encerrar o sistema?",
+                "Ingressos do Teatro POO", JOptionPane.YES_NO_OPTION) == JOptionPane.NO_OPTION);
+        JOptionPane.showMessageDialog(null, "Total de vendas: R$" + vendas.valorArrecadado(),
+                "Teatro POO", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
